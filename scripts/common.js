208902dc0c64cbba7ed29126dbc8a2e539427440
tailam1990 <tailam1990@yahoo.com>
2019-05-29 11:40:31 +0800

    watchToString now handles null/undefined input

--- scripts/common.js
+++ scripts/common.js
@@ -7,7 +7,7 @@ function toArray(e) { return Array.isArray(e) ? e : [e]; }
 function toRadian(d) { return typeof d !== 'number' ? d.mul(Math.PI / 180) : (Math.PI * d / 180); }
 function toDegree(d) { return typeof d !== 'number' ? d.mul(180 / Math.PI) : (180 * d / Math.PI); }
 function between(n, min, max, excl) { return n >= min && n <= max && (!excl || n !== min && n !== max); }
-function watchToString(g) { return Object.keys(g).reduce((s, c) => s.concat(c + ' ').concat((g[c].or ? a => a.ifThenElse('TRUE', 'FALSE') : g[c].mul ? a => a.format('{: 6f}') : a => a)(g[c])).concat('\n'), require('Reactive').val('')); }
+function watchToString(g) { return Object.keys(g).reduce((s, c) => s.concat(c + ' ').concat(g[c] ? (g[c].or ? a => a.ifThenElse('TRUE', 'FALSE') : g[c].mul ? a => a.format('{: 6f}') : a => a)(g[c]) : 'null/undefined').concat('\n'), require('Reactive').val('')); }
 function immediate(f) { require('Time').setTimeout(f, 0); };
 function lookAt(src, tgt) { let RT = require('Reactive'), s = src.transform, t = tgt.transform, v = t.position.sub(s.position); s.rotationX = RT.atan2(v.y, RT.vector(v.x, v.z, 0).magnitude()).neg(); s.rotationY = RT.atan2(v.x, v.z); }
 function ScalarProxy(c, s = []) { let k = ['x', 'y', 'z', 'scaleX', 'scaleY', 'scaleZ', 'rotationX', 'rotationY', 'rotationZ']; s.forEach((n, i) => Object.defineProperty(this, n, { get() { return c.transform[k[i]]; }, set(v) { c.transform[k[i]] = v; } })); }
