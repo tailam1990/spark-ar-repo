96f00b73ed8fd6a5b7b2b7b40f12b3165b07225d
tailam1990 <tailam1990@yahoo.com>
2019-05-29 11:25:51 +0800

    Fixed typo

--- scripts/common.js
+++ scripts/common.js
@@ -10,4 +10,4 @@ function between(n, min, max, excl) { return n >= min && n <= max && (!excl || n
 function watchToString(g) { return Object.keys(g).reduce((s, c) => s.concat(c + ' ').concat((g[c].or ? a => a.ifThenElse('TRUE', 'FALSE') : g[c].mul ? a => a.format('{: 6f}') : a => a)(g[c])).concat('\n'), require('Reactive').val('')); }
 function immediate(f) { require('Time').setTimeout(f, 0); };
 function lookAt(src, tgt) { let RT = require('Reactive'), s = src.transform, t = tgt.transform, v = t.position.sub(s.position); s.rotationX = RT.atan2(v.y, RT.vector(v.x, v.z, 0).magnitude()).neg(); s.rotationY = RT.atan2(v.x, v.z); }
-function ScalarProxy(c, s = []) { let k = ['x', 'y', 'z', 'scaleX', 'ScaleY', 'scaleZ', 'rotationX', 'rotationY', 'rotationZ']; s.forEach((n, i) => Object.defineProperty(this, n, { get() { return c.transform[k[i]]; }, set(v) { c.transform[k[i]] = v; } })); }
+function ScalarProxy(c, s = []) { let k = ['x', 'y', 'z', 'scaleX', 'scaleY', 'scaleZ', 'rotationX', 'rotationY', 'rotationZ']; s.forEach((n, i) => Object.defineProperty(this, n, { get() { return c.transform[k[i]]; }, set(v) { c.transform[k[i]] = v; } })); }
